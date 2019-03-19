bf1388a8c99d986726173338604f93978a337774
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2019-03-19 21:32:27 +0530

    Update udeploy-integration.sh

--- udeploy-integration.sh
+++ udeploy-integration.sh
@@ -2,4 +2,6 @@
 
 cd udclient
 ./udclient -username admin -password admin -weburl https://84.39.32.105:8443 createVersion -component GIT -name TRAVIS_${TRAVIS_BUILD_NUMBER}
-./udclient -username admin -password admin -weburl https://84.39.32.105:8443 addVersionFiles -component GIT -version TRAVIS_${TRAVIS_BUILD_NUMBER} -base ../target/*.war
+mkdir ../basedir
+cp ../target/*.war ../basedir
+./udclient -username admin -password admin -weburl https://84.39.32.105:8443 addVersionFiles -component GIT -version TRAVIS_${TRAVIS_BUILD_NUMBER} -base ../basedir
